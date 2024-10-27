93e263fdf92891bb7da3d254a2e360103d30e3d8
Nafiur01 <nafiurrahman373@gmail.com>
2024-10-27 19:06:25 +0600

    Nav Icons and Search Bar progress

--- src/components/NavIcons.jsx
+++ src/components/NavIcons.jsx
@@ -1,8 +1,40 @@
 "use client";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { CircleUserRound, BellRing, ShoppingCart } from 'lucide-react';
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+
 
 const NavIcons = () => {
-  return <div>NavIcons</div>;
+  const router = useRouter();
+  const [isProfileOpen,setIsProfileOpen] = useState(false);
+  const [isCartOpen,setIsCartOpen] = useState(false);
+  
+  //Temporary 
+  const isLoggedIn = false;
+
+  const handleProfile =() =>{
+    if(!isLoggedIn){
+      router.push('/login')
+    }
+    setIsProfileOpen(prev =>!prev)
+  }
+
+
+  return (
+  <div className="flex items-center gap-4 xl:gap-6">
+    <CircleUserRound height={24} width={24} className="opacity-75"/>
+    {isProfileOpen &&(
+      <div className="">
+        <Link href='/'>Profile</Link>
+        <div className="mt-2 cursor-pointer">logout</div>
+      </div>
+    )}
+    <BellRing height={24} width={24} className="opacity-75"/>
+    <ShoppingCart height={24} width={24} className="opacity-75"/>
+  </div>)
 };
 
 export default NavIcons;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,35 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const SearchBar = () => {
-  return(
-    <form className="">
-      <input type="text" placeholder="Search" />
+  const router = useRouter();
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get("search");
+
+    if (search) {
+      router.push(`/list?name=${search}`);
+    }
+  };
+  return (
+    <form
+      className="flex ic justify-between gap-4 bg-gray-200 p-2 rounded-md flex-1"
+      onSubmit={handleSearch}
+    >
+      <input
+        type="text"
+        name="search"
+        placeholder="Search"
+        className="flex-1 bg-transparent outline-none"
+      />
       <button className="cursor-pointer">
         <Image src="/search.png" alt="" width={16} height={16} />
       </button>
     </form>
-  )
+  );
 };
 
 export default SearchBar;
