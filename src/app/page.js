63ba500932e4c4a609f4690dbaa473344e94c688
Nafiur01 <nafiurrahman373@gmail.com>
2024-11-21 17:48:33 +0600

    Category List and New Category List added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import CategoryList from "@/components/CategoryList";
+import NewCategoryList from "@/components/NewCategory";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 
@@ -9,6 +11,14 @@ const HomePage = () => {
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h2 className="p-2 text-2xl">Featured Products</h2>
         <ProductList />
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Categories</h1>
+        <CategoryList />
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h2 className="p-2 text-2xl">New Category</h2>
+        <NewCategoryList />
       </div>
     </div>
   );
